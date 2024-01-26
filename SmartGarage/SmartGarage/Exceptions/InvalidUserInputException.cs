56c24121d6fa296d5c0dac9405c72a7681575eab
Yavor Manev <yavor.manev.a54@learn.telerikacademy.com>
2024-01-26 14:29:18 +0200

    ServiceTests Added

---
+++ SmartGarage/SmartGarage/Exceptions/InvalidUserInputException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartGarage.Exceptions
+{
+    public class InvalidUserInputException : ApplicationException
+    {
+        public InvalidUserInputException(string message)
+            : base(message)
+        {
+        }
+    }
+}
