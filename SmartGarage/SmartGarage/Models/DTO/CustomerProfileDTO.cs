a3e63f9165f285b066bc0980ecd33ed4f962a69b
Milen Marinov <milen316@gmail.com>
2024-02-20 22:00:31 +0200

    profileController

--- SmartGarage/SmartGarage/Models/DTO/CustomerProfileDTO.cs
+++ SmartGarage/SmartGarage/Models/DTO/CustomerProfileDTO.cs
@@ -5,11 +5,11 @@ namespace SmartGarage.Models.DTO
 {
     public class CustomerProfileDTO
     {
-        public string Username;
-        public string Email;
-        public string PhoneNumber;
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public ICollection<LinkedVehicles> Vehicles;
+        public ICollection<LinkedVehicles> Vehicles { get; set; }
     }
 }
